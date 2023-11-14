15e2eeee901f4624c30c78a4628450e09f0f4fd0
Solune-Petit <123154247+Solune-Petit@users.noreply.github.com>
2023-11-14 10:06:00 +0000

    push

---
+++ php/devinez_un_nombre/functions/DevinerNombre.php
@@ -0,0 +1,18 @@
+<?php
+    $aDeviner = 150;
+    $message = null;
+    $gagne = false;
+
+    if ($value > $aDeviner)
+    {
+        $message = "votre chiffre est trop grand";
+    }else if ($value < $aDeviner)
+    {
+        $message = "votre chiffre est trop petit";
+    }else
+    {
+        $message = "bravo : vous avez deviné le chiffre" . $aDeviner;
+        $gagne = true;
+    }
+    return $gagne
+?>
---
+++ php/devinez_un_nombre/index.php
@@ -0,0 +1,30 @@
+<?php 
+    require_once "php/fonctions.php";
+    $value = 0;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>JOUEZ AVEC NOUS !!!!</h1>
+    </header>
+    <main>
+        <div>
+            <h2>DEVINEZ UN NOMBRE</h2>
+            <form action="guess" method="">
+                <input type="text" id="numberToGuess" name="numberToGuess">
+
+            </form>
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
